d09d20679a198f7f1ca2c66ce36120430ae9786d
LiBingze753 <2603032777@qq.com>
2026-07-20 15:28:28 +0800

    js学习

---
+++ js/demo.js
@@ -0,0 +1,19 @@
+// alert("hello js");
+
+// 变量的声明
+// let a = 10;
+// a = "wadwd";
+
+// 输出到弹窗的值
+// alert(a);
+
+
+// 常量的声明
+const b = 100;
+// b = 200;
+
+//输出到控制台的值
+// console.log(b);
+
+// 输出到文档的值
+document.write(b);
